92e31d00a3f2cdedf7aa9c039a8ac41b84e2239e
atinuke <atinukeadebayo97@gmail.com>
2022-10-19 03:16:49 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabets jn lowercase
+ * followed by a new line
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
